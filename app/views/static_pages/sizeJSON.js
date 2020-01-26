4d2f663fb0ea2e928454d902ed4be38d518ac0da
Taylor Young <taylorjayoung@gmail.com>
2020-01-26 17:46:16 -0500

    any changes?

---
+++ app/views/static_pages/sizeJSON.js
@@ -0,0 +1,59 @@
+var sizes = {
+  "baby": {
+      "age": ["Preemie", "24M"],
+      "category": [
+        "apparel"
+      ]
+        "sizes": []
+    },
+  "toddler"  : {
+    "age": ["2T", "5T"],
+    "sizes": []
+
+  },
+  "kid" : {
+    "age": ["4", "14"],
+    "sizes": []
+
+  },
+  "shoes" : {
+    "sizes": []
+  }
+}
+
+
+"category": [
+  "apparel": {
+      "sizing": [
+      [ {"size": "P up to 5 lbs"}, {"height": 'Up to 17"'}, {"weight": "Up to 5 lb"}],
+      [ {"size": "P (Preemie)"}, {"height": 'Up to 18"'}, {"weight": "Up to 6 lb"}],
+      [ {"size": "NB (Newborn)"}, {"height": '18-21.5"'}, {"weight": "6-9 lb"}],
+      [ {"size": "3M"}, {"height": '21.5-24"'}, {"weight": "9-12.5 lb"}],
+      [ {"size": "6M"}, {"height": '24-27"'}, {"weight": "12.5-17 lb"}],
+      [ {"size": "6M"}, {"height": '24-27"'}, {"weight": "12.5-17 lb"}],
+      [ {"size": "9M"}, {"height": '27-28.5"'}, {"weight": "17-21 lb"}],
+      [ {"size": "12M"}, {"height": '28.5-30"'}, {"weight": "21-25 lb"}],
+      [ {"size": "18M"}, {"height": '30-32"'}, {"weight": "25-28 lb"}],
+      [ {"size": "24M"}, {"height": '32-34"'}, {"weight": "28-30 lb"}],
+      [ {"size": }, {"height": }, {"weight": }]
+    ]
+  },
+  "socks": {
+    "sizing": [
+      {'size': "0-3M", "US Shoe Size": "NB/0-1"},
+      {'size': "3-12M", "US Shoe Size": "1-4"},
+      {'size': "12-24M", "US Shoe Size": "4-7"},
+      {'size': "2T-4T", "US Shoe Size":"7-10" },
+      {'size': "2-4", "US Shoe Size": "7-10"},
+      {'size': "4-6", "US Shoe Size": "10-13"},
+      {'size': "4-7", "US Shoe Size": "10-13"},
+      {'size': "6-8", "US Shoe Size": "13-1Y"},
+      {'size': "8 & up", "US Shoe Size": "1Y & up"}
+    ]
+  },
+
+  "tights": {},
+  "hats": {},
+  "gloves": {},
+
+]
